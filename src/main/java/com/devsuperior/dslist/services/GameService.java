e0be1ecefb87c028482473d490a65362516dfde5
infhos <marcos.monteiro.sp@gmail.com>
2023-07-11 22:03:07 -0300

    Aula 2 - completada

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
@@ -31,5 +32,10 @@ public class GameService {
 		return gameDTO;
 	}
 	
-	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> games = gameRepository.searchByList(listId);
+		List<GameMinDTO> dtoGame = games.stream().map(x -> new GameMinDTO(x)).toList();
+		return dtoGame;
+	}	
 }
